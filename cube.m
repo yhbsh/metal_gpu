36a94617461462b5aa705ff203c11f1d19675f24
yhbsh <houssembousmaha.ksu@gmail.com>
2024-10-18 00:57:21 +0300

    clean

--- cube.m
+++ cube.m
@@ -3,141 +3,100 @@
 #import <QuartzCore/QuartzCore.h>
 #import <simd/simd.h>
 
-@interface MetalView : NSView
-@end
-
-@implementation MetalView
-
-id<MTLDevice>                _gpu;
-id<MTLCommandQueue>          _commandQueue;
-id<MTLRenderPipelineState>   _pipelineState;
-id<MTLBuffer>                _positionsBuffer;
-id<MTLBuffer>                _colorsBuffer;
-id<MTLBuffer>                _indicesBuffer;
-id<MTLBuffer>                _angleBuffer;
-id<MTLLibrary>               _library;
-id<MTLFunction>              _vertFunction;
-id<MTLFunction>              _fragFunction;
-id<CAMetalDrawable>          _drawable;
-id<MTLCommandBuffer>         _commandBuffer;
-id<MTLRenderCommandEncoder>  _renderEncoder;
-CAMetalLayer                *_layer;
-NSTimer                     *_timer;
-CFTimeInterval               _time;
-MTLRenderPipelineDescriptor *_renderPipelineDescriptor;
-MTLRenderPassDescriptor     *_renderPassDescriptor;
-
-static const uint16 indices[] = {
-    0, 1, 2,
-    2, 1, 3,
-
-    4, 5, 6,
-    6, 5, 7,
-};
-static const simd_float4 positions[] = {
-    {-0.5, -0.5, 0.0, 1.0},
-    {+0.5, -0.5, 0.0, 1.0},
-    {-0.5, +0.5, 0.0, 1.0},
-    {+0.5, +0.5, 0.0, 1.0},
-
-    {0.0, -0.5, -0.5, 1.0},
-    {0.0, +0.5, -0.5, 1.0},
-    {0.0, -0.5, +0.5, 1.0},
-    {0.0, +0.5, +0.5, 1.0}
-};
-static const simd_float4 colors[] = {
-    {0.0, 0.5, 0.6, 1.0},
-    {1.0, 0.5, 0.6, 1.0},
-};
-
-- (instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    if (!self) {
-        return nil;
-    }
-
-    _time         = CACurrentMediaTime();
-    _gpu          = MTLCreateSystemDefaultDevice();
-    _layer        = [CAMetalLayer layer];
-    _layer.device = _gpu;
-    _commandQueue = [_gpu newCommandQueue];
-    self.layer    = _layer;
-
-    _library      = [_gpu newLibraryWithURL:[[NSURL alloc] initWithString:@"shaders.metallib"] error:nil];
-    _vertFunction = [_library newFunctionWithName:@"vertex_cube_main"];
-    _fragFunction = [_library newFunctionWithName:@"fragment_cube_main"];
-
-    _renderPipelineDescriptor                                 = [[MTLRenderPipelineDescriptor alloc] init];
-    _renderPipelineDescriptor.label                           = @"Pipeline Descriptor";
-    _renderPipelineDescriptor.vertexFunction                  = _vertFunction;
-    _renderPipelineDescriptor.fragmentFunction                = _fragFunction;
-    _renderPipelineDescriptor.colorAttachments[0].pixelFormat = _layer.pixelFormat;
-
-    _pipelineState = [_gpu newRenderPipelineStateWithDescriptor:_renderPipelineDescriptor error:nil];
-
-    _positionsBuffer = [_gpu newBufferWithBytes:positions length:sizeof(positions) options:MTLResourceStorageModeShared];
-    _colorsBuffer    = [_gpu newBufferWithBytes:colors length:sizeof(colors) options:MTLResourceStorageModeShared];
-    _indicesBuffer   = [_gpu newBufferWithBytes:indices length:sizeof(indices) options:MTLResourceStorageModeShared];
-
-    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 / 144.0 target:self selector:@selector(render) userInfo:nil repeats:YES];
-
-    return self;
-}
-
-- (void)render {
-    simd_float1 elapsed = CACurrentMediaTime() - _time;
-    _angleBuffer = [_gpu newBufferWithBytes:&elapsed length:sizeof(elapsed) options:MTLResourceStorageModeShared];
-
-    _drawable      = [_layer nextDrawable];
-    _commandBuffer = [_commandQueue commandBuffer];
-
-    _renderPassDescriptor                                = [MTLRenderPassDescriptor renderPassDescriptor];
-    _renderPassDescriptor.colorAttachments[0].texture    = _drawable.texture;
-    _renderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
-    _renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0);
-
-    _renderEncoder = [_commandBuffer renderCommandEncoderWithDescriptor:_renderPassDescriptor];
-
-    [_renderEncoder setRenderPipelineState:_pipelineState];
-    [_renderEncoder setVertexBuffer:_positionsBuffer offset:0 atIndex:0];
-    [_renderEncoder setVertexBuffer:_colorsBuffer    offset:0 atIndex:1];
-    [_renderEncoder setVertexBuffer:_angleBuffer     offset:0 atIndex:2];
-
-    [_renderEncoder drawIndexedPrimitives:MTLPrimitiveTypeTriangle indexCount:sizeof(indices) / sizeof(indices[0]) indexType:MTLIndexTypeUInt16 indexBuffer:_indicesBuffer indexBufferOffset:0];
-    [_renderEncoder endEncoding];
-
-    [_commandBuffer presentDrawable:_drawable];
-    [_commandBuffer commit];
-}
-
-@end
-
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
         NSApplication *app = [NSApplication sharedApplication];
         [app setActivationPolicy:NSApplicationActivationPolicyRegular];
 
-        NSMenu     *menu = [[NSMenu alloc] init];
+        NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 600, 600)
+                                                       styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable
+                                                         backing:NSBackingStoreBuffered
+                                                           defer:NO];
+        [window setTitle:@"Cube"];
+        [window makeKeyAndOrderFront:nil];
+        [window center];
+        [app activateIgnoringOtherApps:YES];
+
+        id<MTLDevice> device  = MTLCreateSystemDefaultDevice();
+        CAMetalLayer *layer   = [CAMetalLayer layer];
+        layer.device          = device;
+        layer.pixelFormat     = MTLPixelFormatBGRA8Unorm;
+        layer.framebufferOnly = YES;
+
+        [window.contentView setLayer:layer];
+        [window.contentView setWantsLayer:YES];
+
+        NSMenu *menu     = [[NSMenu alloc] init];
         NSMenuItem *item = [[NSMenuItem alloc] init];
         [menu addItem:item];
         [app setMainMenu:menu];
 
-        NSMenu     *appMenu  = [[NSMenu alloc] init];
+        NSMenu *appMenu      = [[NSMenu alloc] init];
         NSMenuItem *quitItem = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
         [appMenu addItem:quitItem];
         [item setSubmenu:appMenu];
 
-        NSWindow  *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 600, 600)
-                                                       styleMask:NSWindowStyleMaskClosable | NSWindowStyleMaskResizable
-                                                         backing:NSBackingStoreBuffered
-                                                           defer:NO];
-        MetalView *view   = [[MetalView alloc] initWithFrame:window.contentView.bounds];
-        [window setContentView:view];
-        [window makeKeyAndOrderFront:nil];
-        [window center];
+        static const uint16 indices[]        = {0, 1, 2, 2, 1, 3, 4, 5, 6, 6, 5, 7};
+        static const simd_float4 positions[] = {{-0.5, -0.5, +0.0, +1.0}, {+0.5, -0.5, +0.0, +1.0}, {-0.5, +0.5, +0.0, +1.0}, {+0.5, +0.5, +0.0, +1.0},
+                                                {+0.0, -0.5, -0.5, +1.0}, {+0.0, +0.5, -0.5, +1.0}, {+0.0, -0.5, +0.5, +1.0}, {+0.0, +0.5, +0.5, +1.0}};
+        static const simd_float4 colors[]    = {{+0.0, +0.5, +0.6, +1.0}, {+1.0, +0.5, +0.6, +1.0}};
 
-        [app activateIgnoringOtherApps:YES];
-        [app run];
+        id<MTLCommandQueue> commandQueue = [device newCommandQueue];
+        id<MTLLibrary> library           = [device newLibraryWithURL:[[NSURL alloc] initWithString:@"shaders.metallib"] error:nil];
+        id<MTLFunction> vertFunction     = [library newFunctionWithName:@"vertex_cube_main"];
+        id<MTLFunction> fragFunction     = [library newFunctionWithName:@"fragment_cube_main"];
+
+        MTLRenderPipelineDescriptor *renderPipelineDescriptor    = [[MTLRenderPipelineDescriptor alloc] init];
+        renderPipelineDescriptor.label                           = @"Pipeline Descriptor";
+        renderPipelineDescriptor.vertexFunction                  = vertFunction;
+        renderPipelineDescriptor.fragmentFunction                = fragFunction;
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = layer.pixelFormat;
+
+        id<MTLRenderPipelineState> pipelineState = [device newRenderPipelineStateWithDescriptor:renderPipelineDescriptor error:nil];
+
+        id<MTLBuffer> positionsBuff = [device newBufferWithBytes:positions length:sizeof(positions) options:MTLResourceStorageModeShared];
+        id<MTLBuffer> colorsBuff    = [device newBufferWithBytes:colors length:sizeof(colors) options:MTLResourceStorageModeShared];
+        id<MTLBuffer> indicesBuff   = [device newBufferWithBytes:indices length:sizeof(indices) options:MTLResourceStorageModeShared];
+
+        while (true) {
+            @autoreleasepool {
+
+                id<CAMetalDrawable> drawable = [layer nextDrawable];
+                if (!drawable) continue;
+
+                simd_float1 elapsed                = CACurrentMediaTime();
+                id<MTLBuffer> angleBuff            = [device newBufferWithBytes:&elapsed length:sizeof(elapsed) options:MTLResourceStorageModeShared];
+                id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
+
+                MTLRenderPassDescriptor *renderPassDescriptor       = [MTLRenderPassDescriptor renderPassDescriptor];
+                renderPassDescriptor.colorAttachments[0].texture    = drawable.texture;
+                renderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
+                renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0);
+
+                id<MTLRenderCommandEncoder> renderCommandEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
+
+                [renderCommandEncoder setRenderPipelineState:pipelineState];
+                [renderCommandEncoder setVertexBuffer:positionsBuff offset:0 atIndex:0];
+                [renderCommandEncoder setVertexBuffer:colorsBuff offset:0 atIndex:1];
+                [renderCommandEncoder setVertexBuffer:angleBuff offset:0 atIndex:2];
+
+                [renderCommandEncoder drawIndexedPrimitives:MTLPrimitiveTypeTriangle
+                                                 indexCount:sizeof(indices) / sizeof(indices[0])
+                                                  indexType:MTLIndexTypeUInt16
+                                                indexBuffer:indicesBuff
+                                          indexBufferOffset:0];
+                [renderCommandEncoder endEncoding];
+
+                [commandBuffer presentDrawable:drawable];
+                [commandBuffer commit];
+
+                NSEvent *event;
+                while ((event = [app nextEventMatchingMask:NSEventMaskAny untilDate:nil inMode:NSDefaultRunLoopMode dequeue:YES])) {
+                    [app sendEvent:event];
+                    [app updateWindows];
+                }
+            }
+        }
     }
     return 0;
 }
--- shaders.metal
+++ shaders.metal
@@ -15,19 +15,19 @@ vertex VertexOut vertex_cube_main(uint                 id        [[vertex_id]],
     const float PI = 3.1415926535897932384626433832795;
     float theta = fmod(angle, PI);
 
-    //float4x4 rotateX = float4x4(
-    //    float4(+1.0, +0.0,        +0.0,         +0.0),
-    //    float4(+0.0, +cos(theta), -sin(theta), +0.0),
-    //    float4(+0.0, +sin(theta), +cos(theta), +0.0),
-    //    float4(+0.0, +0.0,        +0.0,         +1.0)
-    //);
+    float4x4 rotateX = float4x4(
+        float4(+1.0, +0.0,        +0.0,         +0.0),
+        float4(+0.0, +cos(theta), -sin(theta), +0.0),
+        float4(+0.0, +sin(theta), +cos(theta), +0.0),
+        float4(+0.0, +0.0,        +0.0,         +1.0)
+    );
 
-    //float4x4 rotateY = float4x4(
-    //    float4(+cos(theta), +0.0, +sin(theta), +0.0),
-    //    float4(+0.0,        +1.0, +0.0,         +0.0),
-    //    float4(-sin(theta), +0.0, +cos(theta), +0.0),
-    //    float4(+0.0,        +0.0, +0.0,         +1.0)
-    //);
+    float4x4 rotateY = float4x4(
+        float4(+cos(theta), +0.0, +sin(theta), +0.0),
+        float4(+0.0,        +1.0, +0.0,         +0.0),
+        float4(-sin(theta), +0.0, +cos(theta), +0.0),
+        float4(+0.0,        +0.0, +0.0,         +1.0)
+    );
 
     float4x4 rotateZ = float4x4(
         float4(+cos(theta), -sin(theta), +0.0, +0.0),
@@ -36,8 +36,8 @@ vertex VertexOut vertex_cube_main(uint                 id        [[vertex_id]],
         float4(+0.0,        +0.0,        +0.0, +1.0)
     );
 
-    //float4x4 matrix = rotateZ * rotateY * rotateX;
-    float4x4 matrix = rotateZ;
+    float4x4 matrix = rotateZ * rotateY * rotateX;
+    //float4x4 matrix = rotateZ;
 
     vout.position = matrix * positions[id];
     vout.color = colors[id / 4];
@@ -47,30 +47,4 @@ vertex VertexOut vertex_cube_main(uint                 id        [[vertex_id]],
 
 fragment float4 fragment_cube_main(float4 color [[stage_in]]) {
     return color;
-}
-
-struct VertexOutTriangle {
-    float4 position [[position]];
-    float3 color;
-};
-
-vertex VertexOutTriangle vertex_triangle_main(uint                 id        [[vertex_id]], 
-                             const device float2 *positions [[buffer(0)]],
-                             const device float3 *colors    [[buffer(1)]],
-                             const device float  &time      [[buffer(2)]]) {
-    VertexOutTriangle vout;
-    float angle = 4 * sin(time);
-    float4x4 rotation = float4x4(
-        +cos(angle), -sin(angle), +0.0, +0.0,
-        +sin(angle), +cos(angle), +0.0, +0.0, 
-        +0.0,        +0.0,        +1.0, +0.0, 
-        +0.0,        +0.0,        +0.0, +1.0
-    );
-    vout.position = rotation * float4(positions[id] + float2(0.0, 0.1), 0.0, 1.0);
-    vout.color = colors[id];
-    return vout;
-}
-
-fragment float4 fragment_triangle_main(float3 color [[stage_in]]) {
-    return float4(color, 1.0);
 }
