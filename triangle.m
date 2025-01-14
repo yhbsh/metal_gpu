09c80ec7f7ab815456a06af788267e9249c7b894
yhbsh <houssembousmaha.ksu@gmail.com>
2025-01-15 01:30:19 +0300

    simplify code

--- triangle.m
+++ triangle.m
@@ -1,6 +1,7 @@
 #import <Cocoa/Cocoa.h>
 #import <Metal/Metal.h>
 #import <QuartzCore/QuartzCore.h>
+#import <simd/simd.h>
 
 @interface MetalView : NSView
 @end
@@ -10,7 +11,8 @@
 id<MTLDevice>                _gpu;
 id<MTLCommandQueue>          _commandQueue;
 id<MTLRenderPipelineState>   _pipelineState;
-id<MTLBuffer>                _vertBuffer;
+id<MTLBuffer>                _positionsBuffer;
+id<MTLBuffer>                _colorsBuffer;
 id<MTLBuffer>                _timeBuffer;
 id<MTLLibrary>               _library;
 id<MTLFunction>              _vertFunction;
@@ -20,16 +22,23 @@ id<MTLCommandBuffer>         _commandBuffer;
 id<MTLRenderCommandEncoder>  _renderEncoder;
 CAMetalLayer                *_layer;
 NSTimer                     *_timer;
-CFTimeInterval               _time;
 MTLRenderPipelineDescriptor *_renderPipelineDescriptor;
 MTLRenderPassDescriptor     *_renderPassDescriptor;
+CFTimeInterval               _time;
+
+static const simd_float2 positions[] = {
+    {-0.5, -0.5},
+    {+0.5, -0.5},
+    {+0.0, +0.5},
+};
+static const simd_float3 colors[] = {
+    {1.0, 0.0, 0.0},
+    {0.0, 1.0, 0.0},
+    {0.0, 0.0, 1.0},
+};
 
 - (instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    if (!self) {
-        return nil;
-    }
-
+    self  = [super initWithFrame:frame];
     _time = CACurrentMediaTime();
 
     _gpu          = MTLCreateSystemDefaultDevice();
@@ -39,8 +48,8 @@ MTLRenderPassDescriptor     *_renderPassDescriptor;
     self.layer    = _layer;
 
     _library      = [_gpu newLibraryWithURL:[[NSURL alloc] initWithString:@"shaders.metallib"] error:nil];
-    _vertFunction = [_library newFunctionWithName:@"vert_main"];
-    _fragFunction = [_library newFunctionWithName:@"frag_main"];
+    _vertFunction = [_library newFunctionWithName:@"vertex_main"];
+    _fragFunction = [_library newFunctionWithName:@"fragment_main"];
 
     _renderPipelineDescriptor                                 = [[MTLRenderPipelineDescriptor alloc] init];
     _renderPipelineDescriptor.label                           = @"Pipeline Descriptor";
@@ -48,16 +57,18 @@ MTLRenderPassDescriptor     *_renderPassDescriptor;
     _renderPipelineDescriptor.fragmentFunction                = _fragFunction;
     _renderPipelineDescriptor.colorAttachments[0].pixelFormat = _layer.pixelFormat;
 
-    _pipelineState            = [_gpu newRenderPipelineStateWithDescriptor:_renderPipelineDescriptor error:nil];
-    static const float data[] = {-0.3f, -0.3f, +1.0f, +0.0f, +0.0f, +0.3f, -0.3f, +0.0f, +1.0f, +0.0f, +0.0f, +0.3f, +0.0f, +0.0f, +1.0f};
-    _vertBuffer               = [_gpu newBufferWithBytes:data length:sizeof(data) options:MTLResourceStorageModeShared];
-    _timer                    = [NSTimer scheduledTimerWithTimeInterval:1.0 / 144.0 target:self selector:@selector(render) userInfo:nil repeats:YES];
+    _pipelineState = [_gpu newRenderPipelineStateWithDescriptor:_renderPipelineDescriptor error:nil];
+
+    _positionsBuffer = [_gpu newBufferWithBytes:positions length:sizeof(positions) options:MTLResourceStorageModeShared];
+    _colorsBuffer    = [_gpu newBufferWithBytes:colors length:sizeof(colors) options:MTLResourceStorageModeShared];
+
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 / 144.0 target:self selector:@selector(render) userInfo:nil repeats:YES];
     return self;
 }
 
 - (void)render {
-    CFTimeInterval current = CACurrentMediaTime();
-    float          elapsed = current - _time;
+    float elapsed = CACurrentMediaTime() - _time;
+    _timeBuffer   = [_gpu newBufferWithBytes:&elapsed length:sizeof(elapsed) options:MTLResourceStorageModeShared];
 
     _drawable      = [_layer nextDrawable];
     _commandBuffer = [_commandQueue commandBuffer];
@@ -69,11 +80,9 @@ MTLRenderPassDescriptor     *_renderPassDescriptor;
 
     _renderEncoder = [_commandBuffer renderCommandEncoderWithDescriptor:_renderPassDescriptor];
     [_renderEncoder setRenderPipelineState:_pipelineState];
-    [_renderEncoder setVertexBuffer:_vertBuffer offset:0 atIndex:0];
-
-    _timeBuffer = [_gpu newBufferWithBytes:&elapsed length:sizeof(elapsed) options:MTLResourceStorageModeShared];
-    [_renderEncoder setVertexBuffer:_timeBuffer offset:0 atIndex:1];
-
+    [_renderEncoder setVertexBuffer:_positionsBuffer offset:0 atIndex:0];
+    [_renderEncoder setVertexBuffer:_colorsBuffer offset:0 atIndex:1];
+    [_renderEncoder setVertexBuffer:_timeBuffer offset:0 atIndex:2];
     [_renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3];
     [_renderEncoder endEncoding];
 
@@ -86,27 +95,29 @@ MTLRenderPassDescriptor     *_renderPassDescriptor;
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
         NSApplication *app = [NSApplication sharedApplication];
-        [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+        [app setActivationPolicy:NSApplicationActivationPolicyRegular];
 
         NSMenu     *menu = [[NSMenu alloc] init];
         NSMenuItem *item = [[NSMenuItem alloc] init];
         [menu addItem:item];
-        [NSApp setMainMenu:menu];
+        [app setMainMenu:menu];
 
-        NSMenu     *appMenu      = [[NSMenu alloc] init];
-        NSString   *quitTitle    = [NSString stringWithFormat:@"Quit %@", [[NSProcessInfo processInfo] processName]];
-        NSMenuItem *quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitTitle action:@selector(terminate:) keyEquivalent:@"q"];
-        [appMenu addItem:quitMenuItem];
+        NSMenu     *appMenu  = [[NSMenu alloc] init];
+        NSMenuItem *quitItem = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
+        [appMenu addItem:quitItem];
         [item setSubmenu:appMenu];
 
-        NSWindow  *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 600, 600) styleMask:NSWindowStyleMaskClosable | NSWindowStyleMaskResizable backing:NSBackingStoreBuffered defer:NO];
+        NSWindow  *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 600, 600)
+                                                       styleMask:NSWindowStyleMaskClosable | NSWindowStyleMaskResizable
+                                                         backing:NSBackingStoreBuffered
+                                                           defer:NO];
         MetalView *view   = [[MetalView alloc] initWithFrame:window.contentView.bounds];
         [window setContentView:view];
         [window makeKeyAndOrderFront:nil];
         [window center];
 
-        [NSApp activateIgnoringOtherApps:YES];
-        [NSApp run];
+        [app activateIgnoringOtherApps:YES];
+        [app run];
     }
     return 0;
 }
